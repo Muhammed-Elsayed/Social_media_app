bf69568f556bdf95e399ac8bca4c3066e66e4025
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-12 00:35:54 +0300

    Login Api

--- Social_app/Users/models.py
+++ Social_app/Users/models.py
@@ -6,3 +6,4 @@ class CustomUser(AbstractUser):
     gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')], blank=True)
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='media/photos', blank=True, null=True)
+    email =  models.EmailField(blank=True, max_length=254, unique=True)
--- Social_app/Users/views.py
+++ Social_app/Users/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
-
+from rest_framework.exceptions import AuthenticationFailed
+from .models import CustomUser
+import jwt, datetime
 
 class RegisterView(APIView):
   def post(self, request):
@@ -10,3 +12,21 @@ class RegisterView(APIView):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+  
+class LoginView(APIView):
+    def post(self, request):
+       email = request.data['email']
+       password = request.data['password']
+
+       user = CustomUser.objects.filter(email=email).first()
+
+       if user is None:
+          raise(AuthenticationFailed('User not found'))
+       
+
+       if not user.check_password(password):
+          raise(AuthenticationFailed('Incorrect password'))
+       
+       msg = {'msg' : 'Login successful'}
+
+       return Response(msg)
