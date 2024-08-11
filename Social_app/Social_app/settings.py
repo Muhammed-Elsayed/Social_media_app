00a52ef88772662412f5b1270cb0bfe6abbeefd4
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-11 18:23:25 +0300

    Make a Customized User module

--- Social_app/Social_app/settings.py
+++ Social_app/Social_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Users',
+
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,8 @@ DATABASES = {
     }
 }
 
+
+AUTH_USER_MODEL = 'Users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- Social_app/Social_app/urls.py
+++ Social_app/Social_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Users.models import CustomUser
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
