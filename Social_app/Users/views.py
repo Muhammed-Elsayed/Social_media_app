f47a99f61d4986a65765a2a793ceba28376935df
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-12 01:15:21 +0300

    adding jwt login token sending and reciving

--- Social_app/Users/views.py
+++ Social_app/Users/views.py
@@ -26,7 +26,34 @@ class LoginView(APIView):
 
        if not user.check_password(password):
           raise(AuthenticationFailed('Incorrect password'))
-       
-       msg = {'msg' : 'Login successful'}
+       payload = {
+          'id' : user.id,
+          'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=60),
+          'iat' : datetime.datetime.now(datetime.timezone.utc)
+       } 
+       token = jwt.encode(payload, 'secret', algorithm='HS256')
 
-       return Response(msg)
+       response = Response()
+
+       response.set_cookie(key='jwt',value=token, httponly=True)
+       response.data={
+          'jwt':token
+       }
+       return response
+    
+class UserView(APIView):
+
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        user = CustomUser.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
