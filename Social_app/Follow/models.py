c81006cec1a9fcabcbc3454e5c3272f1bb13fd20
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-08-15 19:26:47 +0300

    updating the follow app

--- Social_app/Follow/models.py
+++ Social_app/Follow/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.conf import settings
 
 class Follow(models.Model):
-    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    followed = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='following',  # This user is following others
+        on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        related_name='followers',  # This user is being followed by others
+        on_delete=models.CASCADE
+    )
 
-    class Meta:
-        unique_together = ('follower', 'followed')
+    def __str__(self):
+        return f"{self.follower} follows {self.followed}"
